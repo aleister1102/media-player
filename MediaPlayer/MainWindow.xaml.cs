f8a2fac615fb74d302764b6c1e29c6b561ffc494
Lê Minh Quân <marucube35@gmail.com>
2023-01-14 23:28:34 +0700

    feat: play, pause and reset media

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -7,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace MediaPlayer
 {
@@ -25,12 +27,13 @@ namespace MediaPlayer
         }
 
         private ObservableCollection<File> _files = new();
+        private bool _playing = false;
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             FilesListView.ItemsSource = _files;
         }
-                
+
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
         {
             var browsingScreen = new OpenFileDialog { Multiselect = true };
@@ -50,6 +53,59 @@ namespace MediaPlayer
                     Debug.WriteLine(filePath);
                     _files.Add(new File() { Path = filePath });
                 }
+            }
+        }
+
+        private void FilesListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var selectedMedia = (File)FilesListView.SelectedItem;
+
+            Player.Source = new Uri(selectedMedia.Path, UriKind.Absolute);
+            Stop();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Player.Source is not null && _playing == false)
+            {
+                Play();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        private void Stop()
+        {
+            Player.Stop();
+
+            _playing = false;
+            PlayButtonImage.Source = new BitmapImage(new Uri("images/play.png", UriKind.Relative));
+        }
+
+        private void Play()
+        {
+            Player.Play();
+
+            _playing = true;
+            PlayButtonImage.Source = new BitmapImage(new Uri("images/pause.png", UriKind.Relative));
+        }
+
+        private void Pause()
+        {
+            Player.Pause();
+
+            _playing = false;
+            PlayButtonImage.Source = new BitmapImage(new Uri("images/play.png", UriKind.Relative));
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Player.Source is not null)
+            {
+                Stop();
+                Play();
             }
         }
     }
